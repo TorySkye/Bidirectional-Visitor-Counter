2913f7b214ea83a857f512dc59e2d0dfc584fdeb
Viktorija Alilović <viktorija.alilovic@gmail.com>
2017-09-13 20:22:07 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,3 @@
-/*
- * Bidirectional Visitor Counter.c
- *
- * Created: 28-Jul-17 1:57:53 PM
- * Author : Viktorija Alilovic
- */ 
-
 /* Includes io.h header file where all the Input/Output Registers and its Bits are defined for all AVR microcontrollers */
 #include <avr/io.h>
 /* Defines a macro for the delay.h header file. F_CPU is the microcontroller frequency value for the delay.h header file */
@@ -27,84 +20,69 @@ uint16_t ADC_ch2_value;
 uint16_t ADC_ch3_value;
 char num_in[20];
 char num_out[20];
-char ch2_string[20];
-char ch3_string[20];
+uint8_t inFlag = 0;
+uint8_t outFlag = 0;
 
 /* Function declarations */
 void ADC_ch2();
 void ADC_ch3();
 
-void USART_init (uint16_t baud)
-{
-	uint16_t baudPrescaler;
-	baudPrescaler = (F_CPU/(16UL*baud))-1;
-	UBRRH = (uint8_t)(baudPrescaler>>8);
-	UBRRL = (uint8_t)baudPrescaler;
-	UCSRA &= ~(1<<U2X);
-	UCSRB = (1<<RXEN)|(1<<TXEN);
-	UCSRC = (1<<URSEL)|(1<<UCSZ1)|(1<<UCSZ0);
-}
-
-void USART_send (uint8_t data)
-{
-	while (!(UCSRA &(1<<UDRE)));
-	UDR=data;
-}
-
-uint8_t USART_receive (void)
-{
-	while (!(UCSRA&(1<<RXC)));
-	return UDR;
-}
-
-void USART_message (char *data)
-{
-	while ( *data != '\0')
-	USART_send(*data++);
-}
-
 int main(void)
 {
-	DDRD = 0b00110000;					/* Configure PortD as output */
-	PORTD = 0x00;						/* Set LOW value to PORT D */
-	DDRA = 0x00;						/* Configure PortA as input */
+	DDRD = 0b00110000;				/* Configure PortD as output */
+	PORTD = 0x00;					/* Set LOW value to PORT D */
+	DDRA = 0x00;					/* Configure PortA as input */
 	
-	USART_init (9600);
-	adc_init();						/* ADC initialization */
+	adc_init();					/* ADC initialization */
 	lcd_init(LCD_DISP_ON);
 	
-	lcd_gotoxy(0,0);					/* Set cursor to 1. row (row0) and 1. character */
-	lcd_puts("Welcome!");
+	lcd_gotoxy(0,0);				/* Set cursor to 1. row (row0) and 1. character */
+	lcd_puts("Dobrodosli");
 	_delay_ms(1000);
 	
-	ch2_read = read_adc_channel(2);			    	/* Converting analog output of Digital light sensor */
-	sprintf(ch2_string, "Reference value for 1. photoresistor: %d  \r\n", ch2_read);
-	USART_message(ch2_string);
+	ch2_read = read_adc_channel(2);			/* Converting analog output of Digital light sensor */
 	ch2_ref = ch2_read;
 
-	ch3_read = read_adc_channel(3);			   	 /* Converting analog output of Digital light sensor */
-	sprintf(ch3_string, "Reference value for 2. photoresistor: %d  \r\n", ch3_read);
-	USART_message(ch3_string);
+	ch3_read = read_adc_channel(3);			/* Converting analog output of Digital light sensor */
 	ch3_ref = ch3_read;
-	
-	USART_send(10);
-	USART_send(13);
 
 	/* Start of infinite loop */
 	while (1)
 	{
 		ADC_ch2();
 		ADC_ch3();
-	
+		
 		if (ADC_ch2_value < (ch2_ref - 100))
 		{
-			if (ADC_ch3_value < (ch3_ref - 100))
+			outFlag = 1;
+			
+			if (inFlag == 1)
 			{
-				counter = counter;
-				PORTD |= (1<<PD4);
-				PORTD |= (1<<PD5);
+				counter++;
+				PORTD |= (1<<PD5);	/* Turn ON LED on PD5 */
+				inFlag = 0;
+				outFlag = 0;
+				_delay_ms(200);
+				lcd_gotoxy(0,0);
+				lcd_puts("Direction: -->");
+				
 			}
 			else
+			{
+				counter = counter;
+				lcd_gotoxy(0,0);
+				lcd_puts("Direction:    ");	
+			}
+			sprintf(num_in, "Visitors: %d ", counter);
+			lcd_gotoxy(0,1);
+			lcd_puts(num_in);
+		}
+		
+		else if (ADC_ch3_value < (ch3_ref - 100))
+		{
+			inFlag = 1;
+			
+			if (outFlag == 1)
 			{
 				if (counter > 0)
 				{
@@ -114,56 +92,42 @@ int main(void)
 				{
 					counter = 0;
 				}
-				PORTD |= (1<<PD4);
-				lcd_gotoxy(0,0);			/* Set cursor to 1. row (row0) and 1. character */
+				
+				inFlag = 0;
+				outFlag = 0;
+				PORTD |= (1<<PD4);	/* Turn ON LED on PD4 */
+				_delay_ms(200);
+				lcd_gotoxy(0,0);
 				lcd_puts("Direction: <--");
-				sprintf(num_in, "Number of visitors: %d ", counter);
-				lcd_gotoxy(0,1);			/* Set cursor to 2. row (row1) and 1. character */
-				lcd_puts(num_in);
-				_delay_ms(1000);
-			}
-		}
-		
-		else if (ADC_ch3_value < (ch3_ref - 100))
-		{
-			if (ADC_ch2_value < (ch2_ref - 100))
-			{
-				counter = counter;
-				PORTD |= (1<<PD4);
-				PORTD |= (1<<PD5);
+
 			}
 			else
 			{
-				counter++;		
-				PORTD |= (1<<PD5);
-				lcd_gotoxy(0,0); 
-				lcd_puts("Direction: -->");
-				sprintf(num_out, "Number of visitors: %d", counter);
-				lcd_gotoxy(0,1); 
-				lcd_puts(num_out);
-				_delay_ms(1000);
+				counter = counter;
+				lcd_gotoxy(0,0);
+				lcd_puts("Direction:    ");
+				
 			}
+			sprintf(num_in, "Visitors: %d ", counter);
+			lcd_gotoxy(0,1);
+			lcd_puts(num_in);
 		}
 		else
 		{
 			PORTD = 0x00;
-		}
+			counter = counter;	
+		}			
 	}
 	return 0;
 }
 
 void ADC_ch2()
 {
-	ADC_ch2_value = read_adc_channel(2);			/* Converting analog output of Digital light sensor */	
-	sprintf(ch2_string, "Value of 1. photoresistor: %d  \r\n", ADC_ch2_value);
-	USART_message(ch2_string);
+	ADC_ch2_value = read_adc_channel(2);		/* Converting analog output of Digital light sensor */	
 }
 
 void ADC_ch3()
 {
-	ADC_ch3_value=read_adc_channel(3);			/*Converting analog output of Digital light sensor*/
-	sprintf(ch3_string, "Value of 2. photoresistor: %d  \r\n", ADC_ch3_value);
-	USART_message(ch3_string);
-	USART_send(10);
-	USART_send(13);
+	ADC_ch3_value=read_adc_channel(3);		/*Converting analog output of Digital light sensor*/
+
 }
